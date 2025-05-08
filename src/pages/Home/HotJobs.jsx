5af43a002db9d6fb58aa17bfb97ccae0914d9bc6
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-05-08 18:28:24 +0600

    reconnect with databse

--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import HotJobCard from './HotJobCard';
+import AuthContext from '../../context/AuthContext/AuthContext';
 
 const HotJobs = () => {
+    // const {user}=useContext(AuthContext)
+    // console.log(user.email)
     const [jobs, setJobs] = useState([]);
 
     useEffect(() => {
         fetch('https://job-portal-server-for-recruiter-part3-nu-nine.vercel.app/jobs')
             .then(res => res.json())
             .then(data => {
+                console.log("hotJobs",data)
                 setJobs(data);
             })
     }, [])
 
     return (
         <div>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+            <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
                 {
                     jobs.map(job => <HotJobCard key={job._id} job={job}></HotJobCard>)
                 }
